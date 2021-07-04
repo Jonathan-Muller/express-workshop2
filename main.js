f89dd736f0006e88779206183f76b9629a325dde
elianaRoseman <elianaroseman@carmel6000.amitnet.org>
2021-07-04 16:04:14 +0300

    forms array and post

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ const port = 8080;
 
 
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/login', (req, res) => {
-  console.log(req.query);
+app.post('/login', (req, res) => {
+  console.log(req.body);
 })
 
 app.get('/params', (req, res) => {
