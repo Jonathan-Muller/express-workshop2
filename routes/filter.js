9fa56a79f79b0dea3f39579dfa27bc14c2c28287
hila-livnat <hilalivnat@carmel.amitnet.org>
2021-07-14 16:15:04 +0300

    ctrl shift i

--- routes/filter.js
+++ routes/filter.js
@@ -4,19 +4,19 @@ var router = express.Router();
 const { cache } = require("../jsml/fcache.js");
 
 function getOptions() {
-    let allCats = cache.fetch()
-    const options = allCats.reduce((prev, curr) => {
-      const found = prev.find(item => item === curr.breed)
-      if (!found) prev.push(curr.breed);
-      return prev;
-    }, ["all"])
-    return options
-  }
+  let allCats = cache.fetch()
+  const options = allCats.reduce((prev, curr) => {
+    const found = prev.find(item => item === curr.breed)
+    if (!found) prev.push(curr.breed);
+    return prev;
+  }, ["all"])
+  return options
+}
 
 /* GET cats listing. */
 router.get('/', function (req, res, next) {
   let allCats = cache.fetch()
-  
+
   //filter the cats according to the cat breed
   //write your code here:
 
